1269f301228fb4caae3e5775e9aec080c53a10ea
Chaudhary Sushil <sushil.chaudhary111@gmail.com>
2023-04-15 16:00:44 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+df = pd.read_csv('doc/data.csv')
+
+# Here we are follow the steps to at last visualize the data
+
+# 1: check data info or details
+print('\n\nCheck Data using info mehtod:\n\n', df.info())
+print('\n\nCheck Data using describe mehtod:\n\n', df.describe())
+
+# Data cleaning: empty cells, data in wrong formate, wrong data or dublicate data
+
+# check null vauelse using
+print('\n\nCheck null values using isnull mehtod:\n\n', df.isnull())
+print('\n\nCheck null values using isnull mehtod with sum():\n\n', df.isnull().sum())
+
+# remove null values using dropna(): used inplace=True to set thid df as permanten
+print('\n\nremove null using dropna():\n\n', df.dropna())
+print('\n\nCheck null values using isnull mehtod with sum():\n\n', df.isnull().sum())
+
+# use fillna() to replace the nan value with anyone
+# noraml way to find the mean value of column in which found nan value and relace the nan with mean value.
+mean_value = df['Calories'].mean()
+print('\n\nReplace nan vaue with mean value:\n\n', df['Calories'].fillna(mean_value))
